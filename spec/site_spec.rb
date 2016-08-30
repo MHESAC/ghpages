ed98122381da33eb03ef2ef6a0deefb082c238f9
Bo Turnbow <bo@turnbows.org>
2016-08-30 09:28:04 -0600

    Use a site.github.url locally For better urls on info page, see http://ben.balter.com/jekyll-style-guide/config/

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -32,6 +32,7 @@ describe 'Web site' do
       {
         :external_only => true,
         :cache => { :timeframe => '1h' },
+        :url_ignore => [ /localhost/ ],
         :file_ignore => [ "_site/info.html" ]
       }
     ).run
